1b1de2cf449bb23dc05569f057a0a483e0664f93
Sohel Bootwala <sohel.bootwala@gmail.com>
2016-04-24 11:28:08 -0500

    5.5 complete

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,58 +1,71 @@
-bike_highway = {
-	
-	"foundation_crew" [
-	 	"employee_name"[ 'Jim', 
-	 					'Bill',
-	 					'Yiwei',
-	 					'Fu'
-	 				]
-	 	{"positions"
-	 	 		"full_capacity"=>5,
-	 	 		"open_position"=>1
-	 	}
-	 	"benifits" [
-	 			"Unlimited paid time off",
-	 			"Full-coverage insurance"
-	 		]
-	]
-
-
-		"painting_crew" [
-		"employee_name" [
-				'Jesus',
-			'Jose',
-				'Paul',
-				'Tat'
-			]
-		"positions" ={
-				'full_capacity = 8',
-				'open_position = 4'
-		}
-		"benifits" [
-				"Unlimited paid time off",
-				"Full-coverage insurance"
-			]
-		]
-		
-
-		"night_crew" {
-		"employee_name"[
-				'Curt',
-				'Eric',
-				'Carl',
-				'Izabella'
-			]
-		"positions" {
-				'full_capacity = 4',
-				'open_position  =0'
-		},
-		"benifits" [
-				"2-days paid time off",
-				"No insurance"
-			]
-		}
-
+coffee_shops_halsted = {
+    peets_coffee: {
+        speciality: 'cappuccino',
+        current_employment: {
+            employees: 9,
+            open_positions: 3
+        },
+        	menu: [
+        		'fruits',
+        		'greens',
+        		'grass'],
+        	sizes: [
+        		'extra small',
+        		'small',
+        		'medium']
+    },
+    kristoffers_cafe: {
+        speciality: 'light meals',
+        current_employment: {
+            employees: 6,
+            open_positions: 2
+        },
+        	menu: [
+        		'meat',
+        		'dead animals',
+        		'dead marine life'],
+        	sizes:	[
+        		'large',
+        		'extra large',
+        		'giant jumbo grande large'
+        		]
+    },
+    cafe_descartes: {
+        speciality: 'great coffee',
+        current_employment: {
+            employees: 11,
+            open_positions: 1
+        },
+        	menu: [
+        		'jelly beans',
+        		'gummy worms',
+        		'skittles'],
+			sizes: [	
+				'small',
+				'medium',
+				'large'
+				]
+    }
 }
 
-bike_highway[night_crew][employee_name][0]
+puts coffee_shops_halsted[:cafe_descartes][:current_employment]
+
+puts coffee_shops_halsted[:kristoffers_cafe][:speciality].reverse
+
+puts coffee_shops_halsted[:peets_coffee][:sizes][0]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
