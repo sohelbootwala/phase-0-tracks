f306980b0ca345ed59f1032783ae21b90993e512
Sohel Bootwala <sohel.bootwala@gmail.com>
2016-04-25 17:44:47 -0500

    Release 3 complete

---
+++ ruby/GPS2.2/gps2_2.rb
@@ -0,0 +1,31 @@
+# Pick what kind of data structure we want - Hash
+# Create a list
+# Method to add an item to a list
+# Method to remove an item from the list
+# Method to update the quantity of an item
+# Method to print a list and make it look pretty
+
+
+
+
+
+
+
+
+grocery_store = {
+		'Lemonade' => 2,
+		'Tomatoes' => 3,
+		'Onions' => 1,
+		'Ice Cream' => 4
+	}
+
+
+
+
+grocery_store['beer'] = 9000000
+
+grocery_store.delete('juice')
+grocery_store['juice'] = 100
+
+p grocery_store
+
