b0a8230b5a6ffe10a90d7af29fbe42d0af3a7e99
Sohel Bootwala <sohel.bootwala@gmail.com>
2016-05-01 16:56:13 -0500

    6.4 revised copy

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -6,15 +6,18 @@
 # Dragon has a birth place
 
 
+
+
 class Dragon
 	attr_reader :age, :birth_place
-	attr_accessor :dragon_name
+	attr_accessor :dragon_name, :animal
 
-def initialize(gender, birth_place)
+def initialize(dragon_name, animal,birth_place)
 	puts "Initializing Dragon's death march towards you!!!!"
 	@dragon_name = dragon_name
 	@birth_place = birth_place
 	@stolen_from_people = 1
+	@animal = animal
 end
 
 def breathe_fire
@@ -25,11 +28,11 @@ def flys_away
  	puts "Flap Flap into the sky"
 end
 
-def farmers(animal)
+def farmers
 	puts "That was a some #{animal}!"
 end
  
- def profile(dragon_name) 
+ def profile 
  	puts "I am #{dragon_name}"
  	dragon_name
 end
@@ -39,15 +42,40 @@ def coins_colleted
 end
 
 end
+animal_name_array = []
+dragon_name_array = []
+while true
+puts 'What is Dragon name?'
+dragon_name =gets.chomp
 
-dragon = Dragon.new("mail", "heaven")
+
+if dragon_name =="quit"
+	break
+end
+puts 'What delicious animal would you like to eat fresh from the farm?'
+animal = gets.chomp
+dragon_name_array=dragon_name_array.push(dragon_name)
+animal_name_array=animal_name_array.push(animal)
+
+end
+
+i=0
+
+while i < dragon_name_array.length
+animal_name = animal_name_array[i]
+name = dragon_name_array[i]
+if i>=1
+	puts "----------------------"
+end
+
+dragon = Dragon.new(name, animal_name,"heaven")
 dragon.breathe_fire 
 dragon.coins_colleted
 dragon.flys_away
-dragon.farmers("cows")
-dragon.profile("Darth Vador")
-
-
+dragon.farmers
+dragon.profile
+i+=1
+end
 
 
 
