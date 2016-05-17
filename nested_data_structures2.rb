c203c607348964ac9c1d4d63a210f26ada7ce10d
Sohel Bootwala <sohel.bootwala@gmail.com>
2016-05-16 22:09:15 -0500

    5.5 Coffee shops started

---
+++ nested_data_structures2.rb
@@ -0,0 +1,15 @@
+coffee_shops_halsted = {
+    peets_coffee: {
+        speciality: 'cappuccino',
+        current_employment: {
+            employees: 9,
+            open_positions: 3
+        },
+        	menu: [
+        		'fruits',
+        		'greens',
+        		'grass'],
+        	sizes: [
+        		'extra small',
+        		'small',
+        		'medium']
