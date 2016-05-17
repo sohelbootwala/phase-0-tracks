8983b9399d76e0250ae022851e8d32d68dc68990
Sohel Bootwala <sohel.bootwala@gmail.com>
2016-05-17 11:53:23 -0500

    Initialize and Pseudocode

---
+++ gps2_2.rb
@@ -0,0 +1,35 @@
+# Method to create a list
+# input: string of items separated by spaces (example: "carrots apples cereal pizza")
+# steps: 
+  # [fill in any steps here]
+  # split the list
+  # add each item as a key to hash
+  # set default quantity to 1 as value
+  # print the list to the console [can you use one of your other methods here?]
+# output: [what data type goes here, array or hash?] hash
+
+# Method to add an item to a list
+# input: item name and optional quantity
+# steps: name method
+  # method should be able to take argument and add it to the hash
+# output: hash with updated key-value pairs
+
+# Method to remove an item from the list
+# input: item key to be removed
+# steps:
+  # input key so that it can be removed from hash
+# output: hash with updated key-value pair
+
+# Method to update the quantity of an item
+# input: input item to be updated 
+# steps:
+  # create a method that takes an argument
+  # check if item actually exists
+  # if so, update the value of the key
+# output: hash with updated key-value pair
+
+# Method to print a list and make it look pretty
+# input: hash
+# steps:
+  # use .each to print key-value pairs
+# output: hash
