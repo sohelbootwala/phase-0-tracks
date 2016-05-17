9c63a0bc51da5af0069527bbe1379f12f2b4493c
Sohel Bootwala <sohel.bootwala@gmail.com>
2016-05-16 22:11:02 -0500

    5.5 Complete

--- nested_data_structures2.rb
+++ nested_data_structures2.rb
@@ -29,3 +29,29 @@ coffee_shops_halsted = {
                 'extra large',
                 'giant jumbo grande large'
                 ]
+                    },
+    cafe_descartes: {
+        speciality: 'great coffee',
+        current_employment: {
+            employees: 11,
+            open_positions: 1
+        },
+            menu: [
+                'jelly beans',
+                'gummy worms',
+                'skittles'],
+            sizes: [    
+                'small',
+                'medium',
+                'large'
+                ]
+    }
+}
+
+puts coffee_shops_halsted[:cafe_descartes][:current_employment]
+
+puts coffee_shops_halsted[:kristoffers_cafe][:speciality].reverse
+
+puts coffee_shops_halsted[:peets_coffee][:sizes][0]
+
+
