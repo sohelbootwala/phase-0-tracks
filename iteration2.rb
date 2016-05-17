e521f8d212f95c9d0e6add4d146146f1315a6845
Sohel Bootwala <sohel.bootwala@gmail.com>
2016-05-16 22:03:25 -0500

    5.4 complete

--- iteration2.rb
+++ iteration2.rb
@@ -6,7 +6,52 @@ food_inventory = {
 		:tomatos => 90,
 		:berries => 700
 }
+
+
+food_inventory.each do |food, count|
+	puts "#{count}"
+end
+
 # looping through a hash using .each
+food_inventory.each do |food, count|
+	puts "We have #{count} #{food}"
+end
+puts "=================================="
+
+
 #looping through an array uisng .each
+my_array.each do |study|
+	puts study + " is among the things I love!!!"
+end
+puts "=================================="
+
+
 #looping through an array uisng .map!
+my_array.map! do |study|
+	puts study
+	my_array.delete("redbull")
+	my_array
+end
+puts "=================================="
+
+puts food_inventory[:carrots]
+
+puts "=================================="
+
+
 # looping through a hash using .map
+food_inventory.map { |thing, value| 
+	puts "#{thing} has #{value} in inventory"
+	food_inventory
+}
+
+
+
+
+
+
+
+
+
+
+
