5b8e41ed43d81d774b5034286d208d16a1f8cd5b
Sohel Bootwala <sohel.bootwala@gmail.com>
2016-05-16 20:17:20 -0500

    5.3 pseudo code

---
+++ hashes2.rb
@@ -0,0 +1,7 @@
+# write a program that allows interior designers to enter the details of a given client
+
+# 1. program - should prompt user for information
+# 2. convert any users input into appropriate data type
+# 3. print the hash back out to the screen which the designer has answered all the questions
+# 4. let user update information
+# 5. print the latest version of the hash and exit
