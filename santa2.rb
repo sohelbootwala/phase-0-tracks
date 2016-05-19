f00f5a2b25ce59f40bfd99032c36ce1f380e2b6d
Sohel Bootwala <sohel.bootwala@gmail.com>
2016-05-19 13:00:22 -0500

    Release 1 complete

--- santa2.rb
+++ santa2.rb
@@ -1,20 +1,43 @@
 class Santa
 
-def initialize
-	puts "Initializing Santa instance ..."
+	def initialize(gender, ethnicity)
+		puts "Initializing Santa instance with a gender of #{gender} and ethnicity of #{ethnicity}"
+		@gender = gender
+		@ethnicity = ethnicity
+	end
+
+	def speak 
+		puts "Ho, ho, ho! Haaaappy holidays!"
+	end
+
+	def eat_milk_and_cookies(cookies)
+		puts "That was a good #{cookies}!"
+	end
+
 end
 
-def speak 
-	puts "Ho, ho, ho! Haaaappy holidays!"
-end
+age = 0 
+reindeer_ranking = ["Rudolph",
+					 "Dasher", 
+					 "Dancer", 
+					 "Prancer", 
+					 "Vixen", 
+					 "Comet", 
+					 "Cupid", 
+					 "Donner", 
+					 "Blitzen"]
 
-def eat_milk_and_cookies(cookies)
-	puts "That was a good #{cookies}!"
-end
 
-end
-
-bob = Santa.new
-puts bob.speak
-puts bob.eat_milk_and_cookies("snickerdoodle")
+# bob = Santa.new
+# puts bob.speak
+# puts bob.eat_milk_and_cookies("snickerdoodle")
+# puts bob("woman", "black")
+santas = []
+santas << Santa.new("agender", "black")
+santas << Santa.new("female", "Latino")
+santas << Santa.new("bigender", "white")
+santas << Santa.new("male", "Japanese")
+santas << Santa.new("female", "prefer not to say")
+santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+santas << Santa.new("N/A", "N/A")
 
