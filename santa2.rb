4567d7dbab6dcd3cca12356c7c5ea408cb62b4f6
Sohel Bootwala <sohel.bootwala@gmail.com>
2016-05-19 16:53:43 -0500

    Release 2 complete

--- santa2.rb
+++ santa2.rb
@@ -4,6 +4,16 @@ class Santa
 		puts "Initializing Santa instance with a gender of #{gender} and ethnicity of #{ethnicity}"
 		@gender = gender
 		@ethnicity = ethnicity
+		@age = 0
+		@reindeer_ranking = ["Rudolph",
+					 "Dasher", 
+					 "Dancer", 
+					 "Prancer", 
+					 "Vixen", 
+					 "Comet", 
+					 "Cupid", 
+					 "Donner", 
+					 "Blitzen"]
 	end
 
 	def speak 
@@ -14,18 +24,21 @@ class Santa
 		puts "That was a good #{cookies}!"
 	end
 
+	def celebrate_birthday
+		@age +=1
+	end
+	
+	def get_mad_at
+		@reindeer_ranking
+	end
+	def get_mad_at(reindeer_name)
+		@reindeer_ranking.delete(reindeer_name)
+		@reindeer_ranking.push(reindeer_name)
+	end
 end
 
 age = 0 
-reindeer_ranking = ["Rudolph",
-					 "Dasher", 
-					 "Dancer", 
-					 "Prancer", 
-					 "Vixen", 
-					 "Comet", 
-					 "Cupid", 
-					 "Donner", 
-					 "Blitzen"]
+
 
 
 # bob = Santa.new
@@ -33,11 +46,24 @@ reindeer_ranking = ["Rudolph",
 # puts bob.eat_milk_and_cookies("snickerdoodle")
 # puts bob("woman", "black")
 santas = []
-santas << Santa.new("agender", "black")
+
+santas.push(Santa.new("agender", "black"))
 santas << Santa.new("female", "Latino")
 santas << Santa.new("bigender", "white")
 santas << Santa.new("male", "Japanese")
 santas << Santa.new("female", "prefer not to say")
 santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
 santas << Santa.new("N/A", "N/A")
+reindeer_ranking.each do |deer|
+	puts "I'm #{deer}"
+end
 
+
+bob = Santa.new
+puts bob.get_mad_at("Comet")
+# santas = []
+# example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+# example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+# example_genders.length.times do |i|
+#   santas << Santa.new(example_genders[i], example_ethnicities[i])
+# end
