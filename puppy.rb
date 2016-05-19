e9d7d6011c242d3e8f86b0eff52536de2f87a8e1
Sohel Bootwala <sohel.bootwala@gmail.com>
2016-05-19 11:59:27 -0500

    Initial play with classes

---
+++ puppy.rb
@@ -0,0 +1,61 @@
+BREED #(class)
+St. Bernard#(instance)
+
+CHARACTERISTICS
+Coat: big and fluffy
+Personality: friendly and loving
+Size: Large(baby elephant size)
+
+NOTE
+These dogs needs lots of space to run and be free. They are big animals.
+
+
+
+
+# Everything below is me just playing around with Classes.
+	# def print_out(value)
+	# 	puts "BREED---------------------"
+	# 	puts breed
+	# 	puts "CHARACTERISTICS-----------"
+	# 	puts characteristics
+	# 	puts "BEHAVIOR" 
+	# 	puts behavior
+
+# class Dog
+# end
+
+
+# breed = Dog.new
+# characteristics = Dog.new
+# behavior = Dog.new
+
+# puts Dog.new
+
+# puts characteristics.instance_of?(Dog)
+
+# # breed += "St. Bernard"
+# # characteristics += "Giant Dog"
+# # characteristics += "Max 260 lbs"
+# # behavior += "Clever"
+# # behavior += "Funny"
+# # behavior += "Chill"
+
+# p dog = Dog.new
+
+
+
+# class Dog
+# end
+
+
+# breed = Dog.new
+# characteristics = Dog.new
+# behavior = Dog.new
+
+
+# breed += "St. Bernard"
+# characteristics += "Giant Dog"
+# characteristics += "Max 260 lbs"
+# behavior += ["Clever", "Funny", "Obidiant"]
+
+# dog = Dog.new
