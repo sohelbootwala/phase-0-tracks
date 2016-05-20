12da7f1df4f9258a449d31635c97f523ae65a05e
Sohel Bootwala <sohel.bootwala@gmail.com>
2016-05-19 22:27:51 -0500

    Release 4 complete

--- santa2.rb
+++ santa2.rb
@@ -1,80 +1,21 @@
 class Santa
-	attr_reader  :ethnicity, :age
-	attr_accessor :gender
+	attr_reader  :ethnicity, :reindeer_ranking
+	attr_accessor :gender, :age
 
 	def initialize(gender, ethnicity)
-		puts "Initializing Santa instance with a gender of #{gender} and ethnicity of #{ethnicity}"
-			@gender = gender = 	["Agender",
-						"Androgyne",
-						"Androgynous",
-						"Bigender",
-						"Cis",
-						"Cisgender",
-						"Cis Female",
-						"Cis Male",
-						"Cis Man",
-						"Cis Woman",
-						"Cisgender Female",
-						"Cisgender Male",
-						"Cisgender Man",
-						"Cisgender Woman",
-						"Female to Male",
-						"FTM",
-						"Gender Fluid",
-						"Gender Nonconforming",
-						"Gender Questioning",
-						"Gender Variant",
-						"Genderqueer",
-						"Intersex",
-						"Male to Female",
-						"MTF",
-						"Neither",
-						"Neutrois",
-						"Non-binary",
-						"Other",
-						"Pangender",
-						"Trans",
-						"Trans*",
-						"Trans Female",
-						"Trans* Female",
-						"Trans Male",
-						"Trans* Male",
-						"Trans Man",
-						"Trans* Man",
-						"Trans Person",
-						"Trans* Person",
-						"Trans Woman",
-						"Trans* Woman",
-						"Transfeminine",
-						"Transgender",
-						"Transgender Female",
-						"Transgender Male",
-						"Transgender Man",
-						"Transgender Person",
-						"Transgender Woman",
-						"Transmasculine",
-						"Transsexual",
-						"Transsexual Female",
-						"Transsexual Male",
-						"Transsexual Man",
-						"Transsexual Person",
-						"Transsexual Woman",
-						"Two-Spirit"]
-			@ethnicity = ethnicity = ['black', 
-								'white', 
-								'brown',
-								'yellow', 
-								'beige']
+		puts "Initializing Santa instance......"
 		@age = 0
+		@gender = gender     
+		@ethnicity = ethnicity
 		@reindeer_ranking = ["Rudolph",
-					 "Dasher", 
-					 "Dancer", 
-					 "Prancer", 
-					 "Vixen", 
-					 "Comet", 
-					 "Cupid", 
-					 "Donner", 
-					 "Blitzen"]
+							 "Dasher", 
+							 "Dancer", 
+							 "Prancer", 
+							 "Vixen", 
+							 "Comet", 
+							 "Cupid", 
+							 "Donner", 
+							 "Blitzen"]
 	end
 
 	def speak 
@@ -92,12 +33,35 @@ class Santa
 	def get_mad_at
 		@reindeer_ranking
 	end
+	
 	def get_mad_at(reindeer_name)
 		@reindeer_ranking.delete(reindeer_name)
 		@reindeer_ranking.push(reindeer_name)
 	end
 
 end
+
+ethnicity = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+gender = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+
+
+100.times do   
+	random_santa = Santa.new(gender.sample, ethnicity.sample)   
+	random_santa.age = rand(140)   
+	p random_santa.age   
+	p random_santa.ethnicity   
+	p random_santa.gender 
+end
+
+# i = 0
+# while i < 100
+# 	santa = Santa.new("@gender", "@ethnicity")
+# 	index_of_ethnicity = rand(santa.ethnicity.length)
+# 	p santa.ethnicity[index_of_ethnicity]
+# 	index_of_gender = rand(santa.gender.length)
+# 	p santa.gender[index_of_gender]
+# 	i += 1
+# end
 
 
 
@@ -107,22 +71,22 @@ end
 # puts bob.speak
 # puts bob.eat_milk_and_cookies("snickerdoodle")
 # puts bob("woman", "black")
-santas = []
+# santas = []
 
-santas.push(Santa.new("agender", "black"))
-santas << Santa.new("female", "Latino")
-santas << Santa.new("bigender", "white")
-santas << Santa.new("male", "Japanese")
-santas << Santa.new("female", "prefer not to say")
-santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
-santas << Santa.new("N/A", "N/A")
-reindeer_ranking.each do |deer|
-	puts "I'm #{deer}"
-end
+# santas.push(Santa.new("agender", "black"))
+# santas << Santa.new("female", "Latino")
+# santas << Santa.new("bigender", "white")
+# santas << Santa.new("male", "Japanese")
+# santas << Santa.new("female", "prefer not to say")
+# santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+# santas << Santa.new("N/A", "N/A")
+# reindeer_ranking.each do |deer|
+# 	puts "I'm #{deer}"
+# end
 
 
-bob = Santa.new
-puts bob.get_mad_at("Comet")
+# bob = Santa.new
+# puts bob.get_mad_at("Comet")
 # santas = []
 # example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
 # example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
