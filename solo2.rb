822bfbd0e03be2267038f3addd3343ddbad7eb7e
Sohel Bootwala <sohel.bootwala@gmail.com>
2016-05-20 11:53:03 -0500

    Array and interface update

--- solo2.rb
+++ solo2.rb
@@ -6,8 +6,8 @@
 # Dragon has a birth place
 
 class Dragon
-	
-
+	attr_reader :age, :destination
+	attr_accessor :dragon_name
 
 	def initialize(dragon_name, destination, age)
 		puts "Ha ah ha ha I'm a big mean DRAGON........."
@@ -36,8 +36,39 @@ class Dragon
 	end
 end
 
-dragon = Dragon.new("Computus Trigerous", "Paris", 250)
-dragon.profile
-dragon.flys_away
-dragon.birthday
-dragon.farmers("cows")
+# dragon = Dragon.new("Computus Trigerous", "Paris", 250)
+# dragon.profile
+# dragon.flys_away
+# dragon.birthday
+# dragon.farmers("cows")
+
+dragon_name_array = []
+dragon_food = []
+destination_array = []
+age_array = []
+while true 
+	
+	puts "What is your dragon name? Peck done when you're complete with adding information"
+	dragon_name = gets.chomp
+	
+		if dragon_name == "done"
+			break
+		end
+	
+	puts "What animal would you like to steal from the farm today?"
+	animal = gets.chomp	
+	puts "Where are you flying to today?"
+	destination = gets.chomp
+	puts "How old are you?"
+	age = gets.chomp
+
+	dragon_name_array = dragon_name_array.push(dragon_name)
+	dragon_food = dragon_food.push(animal)
+	destination_array = destination_array.push(destination)
+	age_array = age_array.push(age)
+
+end
+
+
+
+
