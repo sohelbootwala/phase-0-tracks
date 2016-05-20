55b305b267e9ba89f5793e2ae34ee796c770be59
Sohel Bootwala <sohel.bootwala@gmail.com>
2016-05-20 13:50:27 -0500

    6.6 results

---
+++ calculator/calculator.rb
@@ -0,0 +1,15 @@
+class Calculator
+  def add(x,y)
+    results =x + y
+    return results
+  end
+
+  def subtract(x,y)
+    results = x - y
+    return results
+  end
+
+  def multiply(x,y)
+    x * y
+  end
+end
