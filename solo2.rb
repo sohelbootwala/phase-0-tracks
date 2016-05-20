01f831826dce5348c0b9616adbf005e1878e28aa
Sohel Bootwala <sohel.bootwala@gmail.com>
2016-05-20 10:17:42 -0500

    Pseudocode

---
+++ solo2.rb
@@ -0,0 +1,7 @@
+# # Pseudocode
+# Dragon that returns a name
+# Dragon has the ability to fly 
+# Dragon has the ability to steal farmers cattle
+# When the dragon attacks he steals gold
+# Dragon has a birth place
+
