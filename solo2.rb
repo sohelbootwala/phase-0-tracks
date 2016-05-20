045cee2a1827b252aa26e8699f011ddde24613ca
Sohel Bootwala <sohel.bootwala@gmail.com>
2016-05-20 10:45:53 -0500

    Method and state add

--- solo2.rb
+++ solo2.rb
@@ -5,3 +5,35 @@
 # When the dragon attacks he steals gold
 # Dragon has a birth place
 
+class Dragon
+	def initialize(dragon_name, destination, age)
+		puts "Ha ah ha ha I'm a big mean DRAGON........."
+		@dragon_name = dragon_name
+		@destination = destination
+		@age = age
+	end
+
+
+
+
+	def breathe_fire
+	  puts "WHOOOOOOMPH! *sizzle*"
+	end
+
+	def flys_away
+	 	puts "Flap Flap into the sky, heading to #{@destination}!!!!"
+	end
+
+	def farmers(animal)
+		puts "I like eating #{animal} from farmers property!"
+	end
+	 
+	 def profile 
+	 	puts "My name is #{@dragon_name}"
+	end
+end
+
+dragon = Dragon.new("Computus Trigerous", "Paris", 250)
+dragon.profile
+# dragon.farmers("cows")
+dragon.flys_away
