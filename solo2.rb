a37bfc0c060f03bc8eb88142f811aecc14140bdc
Sohel Bootwala <sohel.bootwala@gmail.com>
2016-05-20 13:39:41 -0500

    Save point, need to complete final release

--- solo2.rb
+++ solo2.rb
@@ -32,42 +32,62 @@ class Dragon
 	end
 	def birthday
 		birthday = @age + 1
+		puts birthday
 		puts "That makes #{@dragon_name} #{birthday} years old"
 	end
 end
 
-# dragon = Dragon.new("Computus Trigerous", "Paris", 250)
-# dragon.profile
-# dragon.flys_away
-# dragon.birthday
-# dragon.farmers("cows")
+dragon = Dragon.new("Computus Trigerous", "Paris", 250)
+dragon.profile
+dragon.flys_away
+dragon.birthday
+dragon.farmers("cows")
 
-dragon_name_array = []
-dragon_food = []
-destination_array = []
-age_array = []
-while true 
-	
-	puts "What is your dragon name? Peck done when you're complete with adding information"
-	dragon_name = gets.chomp
-	
-		if dragon_name == "done"
-			break
-		end
-	
-	puts "What animal would you like to steal from the farm today?"
-	animal = gets.chomp	
-	puts "Where are you flying to today?"
-	destination = gets.chomp
-	puts "How old are you?"
-	age = gets.chomp
+# dragon_name_array = []
+# dragon_food_array = []
+# destination_array = []
+# age_array = []
 
-	dragon_name_array = dragon_name_array.push(dragon_name)
-	dragon_food = dragon_food.push(animal)
-	destination_array = destination_array.push(destination)
-	age_array = age_array.push(age)
 
-end
+# while true 
+# 	puts "What is your dragon name? Peck done when you're complete with adding information"
+# 	dragon_name = gets.chomp
+	
+# 		if dragon_name == "done"
+# 			break
+# 		end
+	
+# 	puts "What animal would you like to steal from the farm today?"
+# 	animal = gets.chomp	
+# 	puts "Where are you flying to today?"
+# 	destination = gets.chomp
+# 	puts "How old are you?"
+# 	age = gets.chomp 
+
+# 		dragon_name_array = dragon_name_array.push(dragon_name)
+# 		dragon_food_array = dragon_food_array.push(animal)
+# 		destination_array = destination_array.push(destination)
+# 		age_array = age_array.push(age)
+# end
+
+# i = 0
+
+# while i < dragon_name_array.length
+# 		animal = dragon_food_array[i]
+# 		destination = destination_array[i]
+# 		age = age_array[i]
+# 	if i >= 1
+# 		puts "------------------------"
+# 	end
+
+# 	dragon = Dragon.new(dragon_name, destination, age)
+# 	dragon.breathe_fire
+# 	dragon.flys_away
+# 	dragon.farmers(animal)
+# 	dragon.profile
+# 	dragon.birthday
+# 	i += 1
+# end
 
 
 
