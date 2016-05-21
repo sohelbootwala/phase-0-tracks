4c08a274e535f999de90ce21475cbfff6f03ec97
Sohel Bootwala <sohel.bootwala@gmail.com>
2016-05-21 12:06:28 -0500

    6.2 Release 2 complete

--- puppy2_methods.rb
+++ puppy2_methods.rb
@@ -1,39 +1,110 @@
-class Puppy
+# class Puppy
 
-	def initialize
-		puts "Initializing new puppy instance ..."
-	end
+# 	def initialize
+# 		puts "Initializing new puppy instance ..."
+# 	end
 	
-	def fetch(toy)
-		puts "I brought back the #{toy}!"
-		toy
+# 	def fetch(toy)
+# 		puts "I brought back the #{toy}!"
+# 		toy
+# 	end
+
+# 	def speak(words)
+# 		words.times do 
+# 			puts "Woof!"
+# 	end
+#     end
+
+# 	def roll_over
+# 		puts "*roll over*"
+# 	end
+
+# 	def dog_years(years)
+# 		dog_age = years.to_i * 7
+# 		puts "Your dog age is #{dog_age}."	
+# 	end
+
+# 	def sit(beg)
+# 		puts "*Puppy eyes* please give me some #{beg}!!!!"
+# 		beg
+# 	end
+
+# 	def 
+# end
+
+
+# puppy = Puppy.new
+# puppy.fetch("baconball")
+# puppy.dog_years(9)
+# puppy.speak(8)
+# puppy.roll_over
+# puppy.sit("doggie treats")
+
+class Study
+	def initialize
+		puts "I need my medication for this......."
 	end
 
-	def speak(words)
-		words.times do 
-			puts "Woof!"
-	end
-    end
-
-	def roll_over
-		puts "*roll over*"
+	def back_pack(item)
+		puts "The content in your bag is a #{item}."
 	end
 
-	def dog_years(years)
-		dog_age = years.to_i * 7
-		puts "Your dog age is #{dog_age}."	
+	def snack(food)
+		puts "Only when I have my #{food}, then only can I study."
 	end
 
-	def sit(beg)
-		puts "*Puppy eyes* please give me some #{beg}!!!!"
-		beg
-	end
+end
+
+study_array = []
+50.times {study_array << Study.new}
+
+
+
+
+study_array.each do |study|
+	study = Study.new
+	study.back_pack("laptop")
+	study.snack("fruit")
 end
 
 
-puppy = Puppy.new
-puppy.fetch("baconball")
-puppy.dog_years(9)
-puppy.speak(8)
-puppy.roll_over
-puppy.sit("doggie treats")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
