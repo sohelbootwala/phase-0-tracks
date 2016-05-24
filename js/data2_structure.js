d21b755c66bd5978f95b8726c15d4578836f632c
Sohel Bootwala <sohel.bootwala@gmail.com>
2016-05-24 11:15:04 -0500

    Release 0 complete

---
+++ js/data2_structure.js
@@ -0,0 +1,8 @@
+var horseName = ['Steve', 'Paul', 'Kumar', 'Harold'];
+var horseColor = ['blue', 'green', 'yellow', 'pink'];
+
+horseName.push('Greg');
+horseColor.push('turquize');
+
+console.log(horseName);
+console.log(horseColor);
