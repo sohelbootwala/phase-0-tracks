505a49557795f5732a39ec3473acff11bd348eb8
Sohel Bootwala <sohel.bootwala@gmail.com>
2016-06-08 14:40:43 -0500

    9.2 complete

---
+++ web_dev/client_side_js/htmlassessment/script.js
@@ -0,0 +1,17 @@
+console.log("This script is running!");
+
+var h1 = document.getElementsByTagName("h1");
+h1b = h1[0]
+h1b.style.border = "3px solid red";
+
+function addBlueBorder(event) {
+	event.target.style.border = "5px solid pink";
+}
+
+var para = document.getElementsByClassName("paragraph");
+addEventListener("click", addBlueBorder);
+
+// var kitten = document.createElement("div");
+// document.body.appendChild(kitten);
+// kitten.class.Name = "container";
+// kitten.style.marginTop = "30px";
