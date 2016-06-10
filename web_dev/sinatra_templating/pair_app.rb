ae90506661354391f5e51ec75bae93b9d44dbacb
Sohel Bootwala <sohel.bootwala@gmail.com>
2016-06-10 11:03:34 -0500

    9.5 Complete

---
+++ web_dev/sinatra_templating/pair_app.rb
@@ -0,0 +1,22 @@
+require 'sinatra'
+require 'sqlite3'
+
+db = SQLite3::Database.new("students.db")
+db.results_as_hash = true
+
+get '/' do 
+  @students = db.execute("SELECT * FROM students")
+  erb :pair_home
+end
+
+get'/update/campus' do 
+  erb :pair_update_campus
+end
+
+post '/campus' do 
+  db.execute("UPDATE students SET campus = ? WHERE name = ? ", [params['campus'], params['name']])
+  redirect '/'
+end
+
+
+# Dandre Wiegand, SD
